2354c46cd215d485126cda47a59aa6b03352f4de
TonyHocNguyen <hoc.nguyen.190028@student.fulbright.edu.vn>
2022-03-07 00:00:02 +0700

    example code for common data types in C++

--- DataTypes.cpp
+++ DataTypes.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+# include <inttypes.h>
 using namespace std;
 
 // A function to print integers
@@ -61,5 +62,46 @@ int main() {
     long double ld2 = 245.12435124L; // extended precision 
     printf("%g, %g \n", ld1, ld2); // let the compiler decide the format
 
-    
+    // character types
+    char c = 'h';
+    char16_t c16 = 'E';
+    char32_t c32 = 'L';
+    signed char sc = 'f';
+    unsigned char uc = 'b';
+    wchar_t wc = 'Z';
+    printf("I will print some characters %c%c\n", c, wc);
+
+    // boolean types
+    bool b1 = true, b2 = false;
+    printf("%d is true and %d is false \n", b1, b2);
+
+    // comparison operators
+    printf("long double == char: %d \n", ld1 == c);
+    printf("char16 != char32: %d \n", c16 != c32);
+    printf("char > int: %d \n", c > i2);
+    printf("signed char >= unsigned char: %d\n", sc >= uc);
+
+    // logical operators
+    bool t = true;
+    bool f = false;
+    printf("!true: %d\n", !t); 
+    printf("true && false: %d\n", t && f); 
+    printf("true && !false: %d\n", t && !f);
+
+    // arrays
+    int arr[] = {1, 2, 3, 4}; // initialize an integer array named 'array'
+    int pos1 = arr[0]; //access the array through index
+    printf("First position in the array: %d \n", pos1);
+
+    // a simple for loop
+    for (int i = 0; i < 4; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    // strings
+    char eng_sentence[] = "A quick brown fox jumped over the lazy dog.";
+    char16_t chinese_sentence[] = u"\u4e66\u4e2d\u81ea\u6709\u9ec4\u91d1\u5c4b";
+    // printf("% \n", chinese_sentence);
+    printf(eng_sentence);
 }
